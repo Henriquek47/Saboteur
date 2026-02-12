1278d676a5c5dcec68214434d51115058ac3b20f
Henrique <henriqueramalholima@gmail.com>
2026-02-12 15:47:23 -0300

    Initial Commit

---
+++ Saboteur/SaboteurApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaboteurApp.swift
+//  Saboteur
+//
+//  Created by Henrique Lima on 12/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SaboteurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SaboteurTests/SaboteurTests.swift
@@ -0,0 +1,17 @@
+//
+//  SaboteurTests.swift
+//  SaboteurTests
+//
+//  Created by Henrique Lima on 12/02/26.
+//
+
+import Testing
+@testable import Saboteur
+
+struct SaboteurTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
